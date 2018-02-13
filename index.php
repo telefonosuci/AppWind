6a317170b3d8fd5ea754e7894847529e874f1d92
Enrico <telefonosuci@gmail.com>
2018-02-13 17:26:46 +0100

    changed protocol http

--- index.php
+++ index.php
@@ -20,7 +20,8 @@
         });
         var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
         j.async=true;
-        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        j.src='http://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-WF36PKJ');
 
     </script>
